f7699b0d40a13a29260f985e8958b6f3bac8f5d7
many-to-one <xllxlex@gmail.com>
2024-06-12 13:47:43 +0200

    yes

--- allegro_scraper/allegro_scraper/middlewares.py
+++ allegro_scraper/allegro_scraper/middlewares.py
@@ -101,3 +101,23 @@ class AllegroScraperDownloaderMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info("Spider opened: %s" % spider.name)
+
+
+# ...
+import random
+
+# ...
+
+# class UARotatorMiddleware:
+#     def __init__(self, user_agents):
+#         self.user_agents = user_agents
+
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         user_agents = crawler.settings.get('USER_AGENTS', [])
+#         return cls(user_agents)
+
+#     def process_request(self, request, spider):
+#         user_agent = random.choice(self.user_agents)
+#         request.headers['User-Agent'] = user_agent
+
--- allegro_scraper/allegro_scraper/settings.py
+++ allegro_scraper/allegro_scraper/settings.py
@@ -16,6 +16,15 @@ NEWSPIDER_MODULE = "allegro_scraper.spiders"
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "allegro_scraper (+http://www.yourdomain.com)"
 
+# USER_AGENTS = [
+#     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+#     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+#     'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15'
+# ]
+
+
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
@@ -53,6 +62,16 @@ ROBOTSTXT_OBEY = True
 #DOWNLOADER_MIDDLEWARES = {
 #    "allegro_scraper.middlewares.AllegroScraperDownloaderMiddleware": 543,
 #}
+
+# ...
+
+# DOWNLOADER_MIDDLEWARES = {
+#     # ... Other middlewares
+#     # 'scratest.middlewares.UARotatorMiddleware': 400,
+#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+#     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+# }
+
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
